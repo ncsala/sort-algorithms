96a4cb76877065e7b99a337a96cd380084f75c3b
Nicolás Cáceres Sala <ncsala@gmail.com>
2021-11-15 15:33:51 -0300

    Updating insertion sort

--- insertion-sort.py
+++ insertion-sort.py
@@ -3,14 +3,14 @@
 lista = [7, 5, 3, 8, 4, 10, 1]
 
 for indice in range(1, len(lista)):
-    #Guardamos el elemento actual para que no se pierda
+    #Se guarda el elemento actual para que no se pierda
     actual = lista[indice]
     
-    #Recorremos los elementos a la izquierda del actual con indice mayor a 0 y si el numero a la izquierda es mayor lo vamos sustituyendo
+    #Se recorren los elementos a la izquierda del actual con indice mayor a 0 y si el numero a la izquierda es mayor lo vamos sustituyendo
     while indice > 0 and lista[indice - 1] > actual:
         lista[indice] = lista[indice-1]
         indice -= 1
-    #Guardamos el elemento actual luego de revisar que todos sus elementos a la izquierda no fueran mayores
+    #Se guarda el elemento actual luego de revisar que todos sus elementos a la izquierda no fueran mayores
     lista[indice] = actual
     
 print(lista)
